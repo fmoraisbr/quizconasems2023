a331a45c409e4413818eb5bfb5103511b386a886
Felipe Morais <contato.felipeamorim@gmail.com>
2023-07-07 02:41:16 -0300

    Summary 2

--- js/jogo.js
+++ js/jogo.js
@@ -1,5 +1,5 @@
 var nutricionista = angular.module('nutricionistaApp', []);
-nutricionista.controller('jogoCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval){
+nutricionista.controller('jogoCtrl', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
 
     $scope.perguntasSelecionadas = [];
     $scope.perguntaTela = {};
@@ -7,13 +7,13 @@ nutricionista.controller('jogoCtrl', ['$scope', '$http', '$interval', function($
     $scope.respostasCorretas = 0;
     $scope.enableButton = false;
     $scope.currentView = 1;
-    $scope.countTimeOut = 240;
+    $scope.countTimeOut = 100;
 
     var perguntasLength = perguntas.length;
     var current = 0;
- 
+
     //captura randomicamente as perguntas evitando repeticoes
-    while ($scope.perguntasSelecionadas.length < 5 ){
+    while ($scope.perguntasSelecionadas.length < 5) {
 
         getRandom($scope, perguntasLength);
     }
@@ -22,32 +22,32 @@ nutricionista.controller('jogoCtrl', ['$scope', '$http', '$interval', function($
 
     montaPergunta($scope, current);
 
-    stop = $interval(function() {
+    stop = $interval(function () {
 
-    
-    console.log($scope.countTimeOut);
 
-    if ($scope.countTimeOut <= 0){
-        
+        console.log($scope.countTimeOut);
 
-        swal({
-          title: "Que pena!!",
-          text: "Seu tempo infelizmente terminou. Tente mais tarde!",
-          imageUrl: "img/neutral_face_emoji.png",
-          showConfirmButton: false
-        });
+        if ($scope.countTimeOut <= 0) {
 
-        setTimeout(function(){ window.location.href = "index.html";}, 7000);
-        
-        return false;
-    }else{
-        $scope.countTimeOut--;
-    }
+
+            swal({
+                title: "Que pena!!",
+                text: "Seu tempo infelizmente terminou. Tente mais tarde!",
+                imageUrl: "img/neutral_face_emoji.png",
+                showConfirmButton: false
+            });
+
+            setTimeout(function () { window.location.href = "index.html"; }, 7000);
+
+            return false;
+        } else {
+            $scope.countTimeOut--;
+        }
 
     }, 1000);
 
 
-    $scope.selecionaItem = function(item, index, perguntaTela) {
+    $scope.selecionaItem = function (item, index, perguntaTela) {
 
         $scope.respostaTemporaria.pergunta = perguntaTela;
         $scope.respostaTemporaria.resposta = index;
@@ -56,21 +56,21 @@ nutricionista.controller('jogoCtrl', ['$scope', '$http', '$interval', function($
 
     }
 
-    $scope.processaResposta = function() {
+    $scope.processaResposta = function () {
 
         var questoesConsolidado = {};
 
-        if(localStorage.questoesConsolidado != undefined){
+        if (localStorage.questoesConsolidado != undefined) {
 
             questoesConsolidado = localStorage.questoesConsolidado;
             questoesConsolidado = JSON.parse(questoesConsolidado);
         }
-        
+
         //prcessa a resposta
 
         var consolidadoPorResposta = {};
 
-        if(localStorage.consolidadoPorResposta != undefined){
+        if (localStorage.consolidadoPorResposta != undefined) {
 
             consolidadoPorResposta = localStorage.consolidadoPorResposta;
             consolidadoPorResposta = JSON.parse(consolidadoPorResposta);
@@ -80,21 +80,21 @@ nutricionista.controller('jogoCtrl', ['$scope', '$http', '$interval', function($
 
         var questao = $scope.respostaTemporaria.pergunta.questao;
 
-        if(consolidadoPorResposta[questao] == undefined){
+        if (consolidadoPorResposta[questao] == undefined) {
 
-            consolidadoPorResposta[questao] = {acertos: 0, erros: 0};
+            consolidadoPorResposta[questao] = { acertos: 0, erros: 0 };
 
         }
 
-        if($scope.respostaTemporaria.pergunta.correto == $scope.respostaTemporaria.resposta){
-            
+        if ($scope.respostaTemporaria.pergunta.correto == $scope.respostaTemporaria.resposta) {
+
             $scope.respostasCorretas++;
 
-            consolidadoPorResposta[questao].acertos = consolidadoPorResposta[questao].acertos +1;
+            consolidadoPorResposta[questao].acertos = consolidadoPorResposta[questao].acertos + 1;
 
-        }else{
+        } else {
 
-            consolidadoPorResposta[questao].erros = consolidadoPorResposta[questao].erros +1;
+            consolidadoPorResposta[questao].erros = consolidadoPorResposta[questao].erros + 1;
 
         }
 
@@ -102,59 +102,59 @@ nutricionista.controller('jogoCtrl', ['$scope', '$http', '$interval', function($
 
         localStorage.consolidadoPorResposta = JSON.stringify(consolidadoPorResposta);
 
-        if(current == 4 /*5 perguntas respondidas*/){
+        if (current == 4 /*5 perguntas respondidas*/) {
 
             console.log($scope.respostasCorretas);
             //processo resposta, salvo em localStorage e mando pra página do resultado
             $scope.checarDesempenho();
 
-        }else{
+        } else {
 
-        current++;
+            current++;
 
-        //passa para a proxima pergunta
-        montaPergunta($scope, current);
+            //passa para a proxima pergunta
+            montaPergunta($scope, current);
 
-        $scope.currentView=  $scope.currentView + 1 ;
+            $scope.currentView = $scope.currentView + 1;
         }
 
         $scope.enableButton = false;
 
     }
 
-    $scope.checarDesempenho = function() {
+    $scope.checarDesempenho = function () {
 
         localStorage.acertos = $scope.respostasCorretas;
 
         var cadastroList = []
 
-        if(localStorage.cadastroList != undefined){
+        if (localStorage.cadastroList != undefined) {
 
             cadastroList = localStorage.cadastroList;
             cadastroList = JSON.parse(cadastroList);
         }
-        
+
         console.log(localStorage.cadastroList);
 
         var cadastro = localStorage.cadastro;
 
         cadastro = JSON.parse(cadastro);
 
-        cadastro.respostasCorretas = $scope.respostasCorretas; 
+        cadastro.respostasCorretas = $scope.respostasCorretas;
 
         cadastroList.push(cadastro);
 
         localStorage.cadastroList = JSON.stringify(cadastroList);
 
         //return false;
-        setTimeout(function(){ }, 500);
+        setTimeout(function () { }, 500);
 
-        if($scope.respostasCorretas >= 3 /*3 perguntas acertadas*/){
+        if ($scope.respostasCorretas >= 3 /*3 perguntas acertadas*/) {
 
             //redirect success
             window.location = "sucesso.html"
 
-        }else{
+        } else {
 
             //redirect tente na proxima
             window.location = "tente_proxima.html"
@@ -166,9 +166,9 @@ nutricionista.controller('jogoCtrl', ['$scope', '$http', '$interval', function($
 
 function montaPergunta($scope, current) {
 
-   
-   var perguntaVez = $scope.perguntasSelecionadas[current];
-   var perguntaTela = perguntas[perguntaVez];
+
+    var perguntaVez = $scope.perguntasSelecionadas[current];
+    var perguntaTela = perguntas[perguntaVez];
 
     console.log(current);
     /*console.log(perguntaVez);*/
@@ -185,29 +185,29 @@ function getRandom($scope, perguntasLength) {
 
     var perguntasSelecionadas = $scope.perguntasSelecionadas;
 
-    if(perguntasSelecionadas.length < 5 /*5 perguntas definidas no requisito*/){
+    if (perguntasSelecionadas.length < 5 /*5 perguntas definidas no requisito*/) {
 
-        random =  Math.floor(Math.random() * perguntasLength);
+        random = Math.floor(Math.random() * perguntasLength);
 
         var insert = true;
-        
-        angular.forEach(perguntasSelecionadas, function(value, key) {
-          
-          if(value == random){
-            insert = false;
-          }
+
+        angular.forEach(perguntasSelecionadas, function (value, key) {
+
+            if (value == random) {
+                insert = false;
+            }
         });
 
-        if (insert){
+        if (insert) {
             $scope.perguntasSelecionadas.push(random);
         }
 
     }
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("body").on("click", ".element-animation1", function() {
+    $("body").on("click", ".element-animation1", function () {
 
         console.log(this);
 
