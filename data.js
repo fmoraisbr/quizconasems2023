ee01328aabebdefa1f25a447f8376d491bf0ce35
Felipe Morais <contato.felipeamorim@gmail.com>
2023-07-12 16:23:50 -0300

    Update data.js

--- data.js
+++ data.js
@@ -14,7 +14,7 @@ var perguntas = [
     "C"
   ],
   [
-    "Dentre os nove Princípios para a elaboração de ações de Educação Alimentar e Nutricional, marque a alternativa que contem pelo menos 3",
+    "Dentre os nove princípios para a elaboração de ações de Educação Alimentar e Nutricional, marque a alternativa que contêm pelo menos 3",
     "Valorização da cultura alimentar local, Promoção de autocuidado e Sustentabilidade social, ambiental e econômica",
     "Diversidade de cenários de prática, Sistema alimentar e sua integralidade e Avaliação de riscos de doenças crônicas",
     "Promoção do autocuidado, Valorização da cultura alimentar e mapeamento de erros nos hábitos alimentares",
@@ -37,26 +37,26 @@ var perguntas = [
   [
     "A má alimentação e nutrição na infância promovem consequências durante toda a vida, sendo elas:",
     "Subnutrição, desnutrição crônica (baixa estatura para a idade) e desnutrição aguda (baixo peso para a altura)",
-    "Sobrepeso, obesidade e hábitos aliment ares saudáveis",
+    "Sobrepeso, obesidade e hábitos alimentáres saudáveis",
     "Baixo desenvolvimento cognitivo, deficiências em micronutrientes e melhora do desempenho escolar",
     "A"
   ],
   [
     "São objetivos das ações de Educação Alimentar e Nutricional, exceto",
-    "Garantir o Direito humano a Alimentação Adequada e Saudável",
+    "Garantir o Direito Humano à Alimentação Adequada e Saudável",
     "Diálogo junto a indivíduos e grupos populacionais",
     "Promover o hábito de dietas mais restritivas na alimentação visando diminuição do consumo de industrializados.",
     "C"
   ],
   [
     "O diálogo entre profissionais e usuários dos serviços de APS é uma característica comum a todas essas práticas. Nessas diferentes interações ocorrem os atos de:",
-    "informar, modificar e direcionar",
-    "orientar, modificar e educar",
-    "informar, orientar e educar",
+    "Informar, modificar e direcionar",
+    "Orientar, modificar e educar",
+    "Informar, orientar e educar",
     "C"
   ],
   [
-    "Dentro da realidade para as práticas de orientação sobre alimentação adequada e saudável-na APS encontram-se:",
+    "Dentro da realidade para as práticas de orientação sobre alimentação adequada e saudável na APS encontram-se:",
     "Renda adequada, disponibilidade de tempo e boa escolaridade",
     "Alto preço de alimentos saudáveis, distância das feiras livres e dificuldade de acesso à UBS",
     "Habilidades culinárias, boa escolaridade e hábitos alimentares adequados",
@@ -71,13 +71,13 @@ var perguntas = [
   ],
   [
     "Diante da multicausalidade do processo saúde-doença e a complexidade das situações de vida de indivíduos, famílias e comunidades, apenas o médico da APS possui responsabilidade direta na oferta de cuidados que considerem as singularidades dos sujeitos em determinado território para possibilitar as respostas mais oportunas às diferentes situações, com o objetivo de ampliar o acesso à RAS e ofertar uma atenção integral à saúde.",
-    1,
-    0,
+    "Verdadeiro",
+    "Falso",
     "Nem falso e nem verdadeiro",
     "B"
   ],
   [
-    "Qual o sistema que monitora o estado nutricional e marcadores de Consumo alimentar na APS?",
+    "Qual o sistema que monitora o estado nutricional e marcadores de consumo alimentar na APS?",
     "E-SUS-APS",
     "SISVAN",
     "SISAB",
@@ -98,17 +98,17 @@ var perguntas = [
     "C"
   ],
   [
-    "Você sabe quais fontes de incentivos financeiros do Ministérios da Saúde e outros, existem para fortalecer direta ou indiretamente a Política Nacional de Alimentação e Nutrição (PNAN)?",
-    "inanciamento de Alimentação e Nutrição (FAN), Estruturação Vigilância Alimentar e Nutricional (VAN), Programa Crescer Saudável (PCS) e Programa Saúde na Escola (PSE)",
+    "Você sabe quais fontes de incentivos financeiros dos Ministérios da Saúde e outros, existem para fortalecer direta ou indiretamente a Política Nacional de Alimentação e Nutrição (PNAN)?",
+    "Financiamento de Alimentação e Nutrição (FAN), Estruturação Vigilância Alimentar e Nutricional (VAN), Programa Crescer Saudável (PCS) e Programa Saúde na Escola (PSE)",
     "Financiamento de Alimentação e Nutrição (FAN), Estruturação Vigilância Alimentar e Nutricional (VAN) e Programa Crescer Saudável (PCS) ",
     "Financiamento de Alimentação e Nutrição (FAN), Estruturação Vigilância Alimentar e Nutricional (VAN), Programa Crescer Saudável (PCS), Programa Saúde na Escola (PSE), Índice de Gestão Descentralizado Municipal (IGD-M), Estratégia PROTEJA e Estratégia Amamenta e Alimenta Brasil (EAAB)",
     "C"
   ],
   [
     "Quantas diretrizes existe na nova Política Nacional de Alimentação e Nutrição (PNAN)?",
-    3,
-    2,
-    9,
+    "3 (três)",
+    "2 (dois)",
+    "9 (nove)",
     "C"
   ],
   [
@@ -134,9 +134,9 @@ var perguntas = [
   ],
   [
     "Cite um dos atributos essenciais da atenção primária à saúde?",
-    "responsabilidade",
-    "vigilância",
-    "integralidade",
+    "Responsabilidade",
+    "Vigilância",
+    "Integralidade",
     "C"
   ],
   [
@@ -150,21 +150,21 @@ var perguntas = [
     "Quais são as estratégias utilizadas na nutrição na atenção básica de saúde?",
     "Avaliação do estado nutricional, educação alimentar e nutricional.",
     "Realização de cirurgias bariátricas e colocação de balão intragástrico.",
-    "Fornecimento exclusivo de suplementos nutricionais e Prescrição de medicamentos para perda de peso.",
+    "Fornecimento exclusivo de suplementos nutricionais e prescrição de medicamentos para perda de peso.",
     "A"
   ],
   [
     "Como o nutricionista pode atuar na Atenção Primária à Saúde? ",
     "Na coordenação, planejamento e execução das políticas públicas relacionadas à alimentação e nutrição, especialmente na atenção nutricional do SUS, para ampliação da promoção da alimentação adequada e saudável e da saúde no município. ",
     "Fazendo atendimentos individuais apenas nas UBS",
-    "Auxiliando da dispensação de formulas enterais das farmácias centrais",
+    "Auxiliando na dispensação de fórmulas enterais das farmácias centrais",
     "A"
   ],
   [
-    "A portaria 635/2023 DG/MS trouxe de volta o modelo de atendimento dos NASF, através da instalação das equipes emulti. É importante ter o nutricionista nessas equipes por quê? ",
+    "A portaria 635/2023 DG/MS trouxe de volta o modelo de atendimento dos NASF, através da instalação das equipes eMulti. É importante ter o nutricionista nessas equipes por quê? ",
     "O nutricionista deve ser o protagonista, indutor e qualificador dos cuidados em alimentação e nutrição na APS, a presença desse profissional deixa o serviço prestado mais resolutivo e reduz a necessidade de atenção especializada.",
-    "Não é necessário que o seja o protagonista, indutor e qualificador dos cuidados em alimentação e nutrição na APS, uma vez que todos os profissionais que atuam na APS são responsáveis pelos cuidados em alimentação e nutrição. ",
-    "O nutricionista não está contemplado nas emulti.",
+    "Não é necessário que seja o protagonista, indutor e qualificador dos cuidados em alimentação e nutrição na APS, uma vez que todos os profissionais que atuam na APS são responsáveis pelos cuidados em alimentação e nutrição. ",
+    "O nutricionista não está contemplado nas eMulti.",
     "A"
   ]
 ];
